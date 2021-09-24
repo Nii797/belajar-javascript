9e04997065682a374ed823fd5ac4ad60d523ad7a
Nii797 <Phanterasonpardus797@gmail.com>
2021-09-24 23:28:14 +0700

    select all

---
+++ script/script.js
@@ -0,0 +1,136 @@
+// Step 1 Percobaan
+// variabel
+// var bilangan1 = 1, bilangan12 = 10;
+// var tambah = bilangan1 + bilangan12; 
+
+// document.writeln(bilangan1 + " + " + bilangan12 + " = " + tambah);
+// document.writeln('<br>');
+// document.writeln(0b1111010);
+// document.writeln("<br>");
+
+// var kondisi = "Hujan";
+// if(kondisi === "Hujan"){
+//     document.writeln("Memakai Payung");
+// } else if(kondisi === "Panas") {
+//     document.writeln("tidak memakai payung");
+// }  
+
+// document.writeln("<br>");
+// document.writeln("<textarea cols='50' rows='10'>");
+// document.writeln("Ini \nadalah " + true);
+// document.writeln("\t" + false);
+// document.write("\\Hello\\Soni\n");
+// document.write('this is javascript');
+// document.writeln("</textarea>");
+
+
+// Variable
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh"); // tidak kondusif
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+// document.writeln("Soni Hidayatulloh");
+// document.writeln("<br>");
+
+
+// var fullName, firstName, midleName, lastName;
+// fullName = "Leon Scott Kennedy";
+// firstName = "Leon";
+// middleName = "Scott";
+// lastName = "James";
+
+// document.write(fullName);
+// document.write("<br>");
+// document.write(firstName + " " + lastName);
+
+//==============================================
+
+// var fullName = "Soni Hidayatulloh Ganteng";
+// var firstName = "Soni";
+// var middleName = "Hidayatulloh";
+// var lastName = "Ganteng";
+
+// document.writeln(fullName);
+// document.writeln("<br>");
+// document.writeln("======================");
+// document.writeln("<br>");
+// document.writeln(firstName);
+// document.writeln("<br>");
+// document.writeln(middleName);
+// document.writeln("<br>");
+// document.writeln(lastName);
+// document.writeln("<br>");
+// document.writeln("<br>");
+// document.writeln("======================");
+// document.writeln("<br>");
+// document.writeln(fullName);
+// document.writeln("<br>");
+// document.writeln(fullName);
+// document.writeln("<br>");
+// document.writeln(fullName);
+// document.writeln("<br>");
+// document.writeln(fullName);
+// document.writeln("<br>");
+
+// var fullName = "Soni Hidayatulloh";
+// document.writeln(fullName);
+// document.writeln("<br>");
+// document.writeln(fullName);
+// document.writeln("<br>");
+
+document.writeln("==================== varaibel let ==========================")
+let fullName = "Soni Hidayatulloh Ganteng";
+let firstName = "Soni";
+let middleName = "Hidayatulloh";
+let lastName = "Ganteng";
+
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln("======================");
+document.writeln("<br>");
+document.writeln(firstName);
+document.writeln("<br>");
+document.writeln(middleName);
+document.writeln("<br>");
+document.writeln(lastName);
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("======================");
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+
+fullName = "Soni Hidayatulloh";
+document.writeln(fullName);
+document.writeln("<br>");
+document.writeln(fullName);
+document.writeln("<br>");
+
+document.writeln("<br>")
+document.writeln("========= varaibel const ============");
+
+const application = "Belajar Javascript";
+application = "Gak mau belajar";
+
+document.writeln(application);

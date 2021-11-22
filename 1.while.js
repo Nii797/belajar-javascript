9b81b970a6963c3089facfce14af8065dcc82373
Nii797 <Phanterasonpardus797@gmail.com>
2021-11-22 19:27:15 +0700

    update while and for  latihan

--- 1.while.js
+++ 1.while.js
@@ -1,7 +1,22 @@
-let angkot = 1;
-while (angkot <= 10) {
-    console.info(`Angkot No. ${angkot} beropasi dengan baik.`);
-    angkot++;
-}
+// {    
+//     let angkot = 1;
+//     while (angkot <= 10) {
+//         console.info(`Angkot No. ${angkot} beropasi dengan baik.`);
+//         angkot++;
+//     }
+// }
 
-console.info("Hello World");
+console.log('\n');
+console.log('\n');
+
+// menggunakan while dan for 
+{
+    // let jmlAngkot = 10;
+    // let angkotBeroperasi = 6;
+    // let noAngkot = 1;
+
+    // while (noAngkot <= angkotBeroperasi) {
+        // console.info(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
+        // noAngkot++;
+    // }
+}

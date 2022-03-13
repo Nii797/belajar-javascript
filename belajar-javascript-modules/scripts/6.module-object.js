4725e6c7462db525fe73ef2a76a4a21ccb85305a
Nii797 <phanterasonpardus797@gmail.com>
2022-03-13 22:49:47 +0700

    add module object

---
+++ belajar-javascript-modules/scripts/6.module-object.js
@@ -0,0 +1,13 @@
+export const firstName = "Soni";
+export const lastName = "Hidayatulloh";
+export const aliasName = "Nii";
+
+export function hello () {
+    console.info(`Hello from ${firstName}`);
+}
+
+export class Person {
+    constructor() {
+        this.name = "Ririn";
+    }
+}

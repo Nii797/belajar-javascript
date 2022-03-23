eddd6728d9856d450e8d0d86048d512be8b8860e
Nii797 <phanterasonpardus797@gmail.com>
2022-03-23 07:28:56 +0700

    modified calculator

--- calculator/main.js
+++ calculator/main.js
@@ -1,2 +1,18 @@
-const title = document.getElementsById("#title");
-title.textContent = "Javascript Calclulator";
+// let title = document.getElementById("title");
+// title.textContent = "Calculator Javascript";
+
+// console.info("Hello Soni");
+
+function display(val) {
+    document.getElementById("textval").value += val
+}
+
+function evaluate() {
+    let x = document.getElementById("textval").value
+    let y = eval(x)
+    document.getElementById("textval").value = y
+}
+
+function clr() {
+    document.getElementById("textval").value = ""
+}
